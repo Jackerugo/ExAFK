80253a58b7d5d418d80cbef439804d8e05bff878
Jack <37919015+Jackerugo@users.noreply.github.com>
2024-06-01 04:41:13 +0100

    bstats toggle option

--- src/main/java/jackerugo/exafk/ExAFK.java
+++ src/main/java/jackerugo/exafk/ExAFK.java
@@ -49,12 +49,17 @@ public class ExAFK extends JavaPlugin implements Listener {
             getServer().getPluginManager().disablePlugin(this);
         }
 
+        if (getConfig().getString("bstats").equalsIgnoreCase("true")) {
+            int pluginId = 22098; //
+            Metrics metrics = new Metrics(this, pluginId);
+            getLogger().info("bstats logging enabled. Thank you!");
+        } else {
+            getLogger().info("bstats logging disabled.");
+        }
+
         if (getServer().getPluginManager().isPluginEnabled(this)) {
             getLogger().info("Successfully loaded! Took " + (System.currentTimeMillis() - startTime) + "ms.");
         }
-
-        int pluginId = 22098; //
-        Metrics metrics = new Metrics(this, pluginId);
 
     }
 
